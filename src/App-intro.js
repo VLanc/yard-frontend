1af7c743432e9550a310edb3c2c9f65fb502041c
VLanc <vlancv@gmail.com>
2017-06-23 15:52:46 +0300

    Added routing and props, deleted unnecessary files

--- src/App-intro.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default () => {
-  return (
-    <p className="AppIntro">
-      To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
-  );
-};
--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
+import styled from "styled-components";
 /*import './style.css';*/
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import Header from './Header';
-import Logo from './Logo';
-import Cards from './Cards';
-import Discover from './Discover';
 import Footer from './Footer';
+
+import Complexes from './Complexes/List';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <Logo />
-        <Discover />
-        <Cards />
-        <Footer />
-      </div>
+      <Router>
+        <div className="App">
+          <Header />
+          <Route exact path="/" component={Complexes} />
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
